7f7cda7e64cbb2c4e8e9ef6db28a4daef6d2ee11
Fyz <Mrfuri54@gmx.fr>
2021-02-23 17:32:48 +0100

    Let's start !

---
+++ src/main/java/fr/emmuliette/meuporgcraft/MeuporgCraft.java
@@ -0,0 +1,13 @@
+package fr.emmuliette.meuporgcraft;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MeuporgCraft implements ModInitializer{
+
+	@Override
+	public void onInitialize() {
+		System.out.println("A Meow Meow Organization Project.");
+		
+	}
+
+}
